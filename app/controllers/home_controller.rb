f8cc2f4dda148222290308f6e03e88b4fa892090
Sunny <sunnyinvadeher@gmail.com>
2018-05-08 00:44:10 +0800

    Display some coins info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
+require "net/http"
+require "json"
+
 class HomeController < ApplicationController
   def index
+    url = "https://api.coinmarketcap.com/v2/listings/?"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @ID_List = JSON.parse(response)
+
+		url = 'https://api.coinmarketcap.com/v2/ticker/'
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+
+		@coins = JSON.parse(response)
+    @my_coins = ["BTC","ETH","XRP"]
   end
 
 	def about
